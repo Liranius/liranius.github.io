7cc5da0115a4b8b7ebb111bb0c2efa600556002f
Liranius <prince.lich@gmail.com>
2017-03-08 15:48:16 +0800

    Remove improper files

--- MBNews/public/js/interfaces.js
+++
@@ -1,2 +0,0 @@
-System.register([],function(e,t){"use strict";t&&t.id;return{setters:[],execute:function(){}}});
-//# sourceMappingURL=interfaces.js.map
--- MBNews/search/content.js
+++
@@ -1,2 +0,0 @@
-System.register(["../../public/js/templates/searchPage","../../public/js/debugData"],function(e,t){"use strict";var a,s;t&&t.id;return{setters:[function(e){a=e},function(e){s=e}],execute:function(){a.searchPage(s.searchPageDbgData)}}});
-//# sourceMappingURL=content.js.map
